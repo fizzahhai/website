82b0455a9a55562830d6f70a695b1a615fa22b59
Fizzah Haider <fizzahhaider@Fizzahs-MacBook-Air.local>
2025-05-09 20:39:27 -0400

    Update about me section and styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,61 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 bg-purple-100">
+      <h1 className="text-4xl font-bold mb-4 text-purple-900">hi! i'm fizzah haider</h1>
+      <p className="text-lg mb-8 text-center max-w-xl text-purple-950">
+        welcome to my site~
+      </p>
+      <section className="mb-8">
+  <h2 className="text-2xl font-semibold mb-2 text-purple-950">about me</h2>
+  <ul className="list-disc list-inside text-purple-950 mt-2">
+    <li>
+      i'm currently an incoming second-year at the university of waterloo studying mathematical physics (bsc). besides my interest in math and physics, i also enjoy listening to music, cooking, and the occasional programming - its why i intend to minor in computing! this site is actually one of my newest projects (a work in progress).
+    </li>
+    <li>
+      proficient in <strong>python, java, racket, figma, autoCAD, adobe, canva, and sketchup</strong>, and always striving to learn new skills!
+    </li>
+  </ul>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-purple-950">projects</h2>
+        <ul className="list-disc list-inside text-purple-950">
+          <li>
+          <strong>reWat</strong>: designed a mobile app during the <strong>impact 2024 hackathon</strong> using <strong>figma</strong>, with a 3d keychain prototype built using <strong>autocad</strong>. focused on promoting sustainable habits among students and incentivizing eco-friendly actions through a points-based system, campus challenges, and local partnerships. presented our fully fleshed-out design prototype to a panel of judges
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li>
+          <strong>snake game</strong>: built a fully functioning custom version of the classic snake game alongside a classmate using <strong>java, canva</strong>, and <strong>figma</strong>. designed custom graphics and implemented game mechanics from scratch.
           </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </ul>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-purple-950">experience</h2>
+        <ul className="list-disc list-inside text-purple-950">
+          <li>
+            <span className="font-bold">undergraduate researcher</span> , york university (2025 - present)<br />
+            creating an AI timeline model under dr ____ for _____ using <strong>python</strong>. 
+          </li>
+          <li>
+          <span className="font-bold">team member</span> , waterloo rocketry (2024 - present)<br />
+          contributed to collaborative engineering discussions and design reviews focused on propulsion + RocketCAN
+          developed introductory understanding of rocketry principles and design processes through exposure to <strong>CAD modeling</strong>, simulation tools, and test planning.
+          </li>
+          <li>
+            <span className="font-bold">front end developer</span> , intelligent learning machines (2022 - 2023)<br />
+worked closely with senior employees to translate design concepts into functional systems for users,  designing visually appealing and accessible interactive interfaces. conducted user research and usability testing to ensure seamless interactions, and improved website navigation and usability through user centered design principles, increasing user retention. worked with <strong>figma, adobe, vscode. </strong>
+          </li>
+          <li>
+            <span className="font-bold">tutor</span> (2021 - present)<br />
+            privately tutored high school students STEM subjects (advanced calculus, physics, chemistry, algebra) up to a first-year university level
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2 className="text-2xl font-semibold mb-2 text-purple-950">contact me</h2>
+        <p className="text-purple-950">
+          email: <a href="mailto:f3haider@uwaterloo.ca" className="text-purple-950 underline">f3haider@uwaterloo.ca</a>
+        </p>
+      </section>
     </div>
   );
 }
